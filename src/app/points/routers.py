da696795aed063f09670f2035bccb7db01c0544d
ValikSilchenko <valik.silchenko@gmail.com>
2023-05-13 15:01:23 +0300

    feat: add category search

--- src/app/points/routers.py
+++ src/app/points/routers.py
@@ -8,7 +8,7 @@ from fastapi import Depends, APIRouter
 from src.app.database import get_async_session
 from src.app.points.models import *
 
-router = APIRouter()
+router = APIRouter(tags=["points"], prefix="/point")
 
 
 # @router.get("/")
@@ -64,3 +64,26 @@ async def fill_db(session: AsyncSession = Depends(get_async_session)):
 
     await session.commit()
 
+
+@router.get("/category/{category_str}", status_code=200)
+async def get_point_by_category(category_str: str, session: AsyncSession = Depends(get_async_session)):
+    query = select(category.c.id).filter(category.c.name.ilike(category_str.lower()))
+    result = await session.execute(query)
+    categories = list(map(lambda x: x[0], result.all()))
+
+    if categories:
+        query = select(category_establishment.c.establishment_id).where(
+            category_establishment.c.category_id.in_(categories))
+        result = await session.execute(query)
+        points = list(map(lambda x: x[0], result.all()))
+
+        query = select(
+            establishment.c.coords, establishment.c.name, establishment.c.address, establishment.c.website
+        ).filter(establishment.c.id.in_(points))
+        result = await session.execute(query)
+
+        data = list(map(lambda x: dict(x), result.mappings().all()))
+        for d in data:
+            d["coords"] = list(map(float, d["coords"].split(';')))
+
+        return data
